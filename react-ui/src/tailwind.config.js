3f1d108f3c629a4377c10d3352cd6974767053ad
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-12 08:17:49 -0600

    m

---
+++ react-ui/src/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
